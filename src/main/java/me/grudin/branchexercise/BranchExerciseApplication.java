f320fb065a473c00dcd771bf55157d1068b1ad5f
Grant Rudin <grantmrudin@gmail.com>
2024-03-19 18:02:14 -0500

    Initial commit

---
+++ src/main/java/me/grudin/branchexercise/BranchExerciseApplication.java
@@ -0,0 +1,13 @@
+package me.grudin.branchexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchExerciseApplication.class, args);
+    }
+
+}
