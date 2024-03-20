3944bb4cc11a95f6c8785ed6fc466fac7bd49421
Grant Rudin <grantmrudin@gmail.com>
2024-03-19 21:46:37 -0500

    Validate `GithubProperties`

--- src/main/java/me/grudin/branchexercise/github/GitHubProperties.java
+++ src/main/java/me/grudin/branchexercise/github/GitHubProperties.java
@@ -1,13 +1,17 @@
 package me.grudin.branchexercise.github;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
 @ConfigurationProperties("github")
+@Validated
 class GitHubProperties {
 
+    @NotBlank
     private String baseUrl;
 }
